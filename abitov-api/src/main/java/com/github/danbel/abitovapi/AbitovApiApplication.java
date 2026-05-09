9d23eed9ec90cf71541352014553725313e8aeb2
DanBel <dan.bel.89@bk.ru>
2026-05-10 02:22:54 +0400

    update

---
+++ abitov-api/src/main/java/com/github/danbel/abitovapi/AbitovApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.danbel.abitovapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbitovApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbitovApiApplication.class, args);
+    }
+
+}
---
+++ abitov-api/src/test/java/com/github/danbel/abitovapi/AbitovApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danbel.abitovapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class AbitovApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
