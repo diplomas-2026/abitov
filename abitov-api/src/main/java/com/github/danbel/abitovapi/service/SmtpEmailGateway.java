1baa1d35f19cee8aadea35f0ea0c1407465f6665
DanBel <dan.bel.89@bk.ru>
2026-05-10 03:28:51 +0400

    update 2026-05-10 03:28:23

--- abitov-api/src/main/java/com/github/danbel/abitovapi/service/SmtpEmailGateway.java
+++ abitov-api/src/main/java/com/github/danbel/abitovapi/service/SmtpEmailGateway.java
@@ -1,12 +1,10 @@
 package com.github.danbel.abitovapi.service;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(prefix = "app.mail", name = "enabled", havingValue = "true")
 public class SmtpEmailGateway implements EmailGateway {
 
     private final JavaMailSender mailSender;
