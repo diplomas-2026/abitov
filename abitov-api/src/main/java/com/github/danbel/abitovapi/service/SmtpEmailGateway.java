ce993f88077c2f5853f5af5a8c0c09035b8cbae3
DanBel <dan.bel.89@bk.ru>
2026-05-10 06:44:37 +0400

    update 2026-05-10 06:44:07

--- abitov-api/src/main/java/com/github/danbel/abitovapi/service/SmtpEmailGateway.java
+++ abitov-api/src/main/java/com/github/danbel/abitovapi/service/SmtpEmailGateway.java
@@ -1,22 +1,25 @@
 package com.github.danbel.abitovapi.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SmtpEmailGateway implements EmailGateway {
 
     private final JavaMailSender mailSender;
 
-    public SmtpEmailGateway(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    @Value("${spring.mail.username}")
+    private String fromEmail;
 
     @Override
     public void send(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
+        message.setFrom(fromEmail);
         message.setSubject(subject);
         message.setText(body);
         mailSender.send(message);
